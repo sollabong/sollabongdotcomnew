be5aba671b9186cfb4fbf98310379069b51e64dd
Gabor Soltesz-Keri <61150302+sollabong@users.noreply.github.com>
2020-10-19 16:55:20 +0200

    recommendation + projects site

--- index.js
+++ index.js
@@ -38,3 +38,54 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "flex";
   dots[slideIndex-1].className += " active";
 }
+
+/* Recommendation  slider */
+
+var slideIndexB = 1;
+showSlidesB(slideIndexB);
+
+// Next/previous controls
+function plusSlidesB(n) {
+  showSlidesB(slideIndexB += n);
+}
+
+// Thumbnail image controls
+function currentSlideB(n) {
+  showSlidesB(slideIndexB = n);
+}
+
+function showSlidesB(n) {
+  var i;
+  var slidesB = document.getElementsByClassName("mySlidesB");
+  var dotsB = document.getElementsByClassName("dotB");
+  if (n > slidesB.length) {slideIndexB = 1}
+  if (n < 1) {slideIndexB = slidesB.length}
+  for (i = 0; i < slidesB.length; i++) {
+      slidesB[i].style.display = "none";
+  }
+  for (i = 0; i < dotsB.length; i++) {
+    dotsB[i].className = dotsB[i].className.replace(" active", "");
+}
+  slidesB[slideIndexB-1].style.display = "flex";
+  dotsB[slideIndexB-1].className += " active";
+}
+
+  /*Scroll to top*/
+
+  mybutton = document.getElementById("myBtn");
+
+  window.onscroll = function() {scrollFunction()};
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
